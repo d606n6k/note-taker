0365a8d35018ccbfb6122562e74994d4aa18b646
William Lucht <aaronlucht@gmail.com>
2021-04-18 13:39:49 -0700

    fixed the readonly bug on the index.js renderActiveNote function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const db = require('../db/db.json');
+const { nanoid } = require("nanoid");
 
 // This module exports a function which accepts an Express app object and
 // and sets up the api routes.
@@ -9,39 +10,47 @@ module.exports = function (app) {
     // read notes data from file
     fs.readFile('./db/db.json', "utf8", (err, data) => {
        if (err) throw err;
-       console.log(data);
-        // db
-        // .then((data)=> {
-        //     res.json(data);
-        // })
-        // .catch((err) => {
-        //     // send error response
-        //     return res.status(500).end();
-        // })
+    //    console.log(data);
+       res.json(JSON.parse(data)); 
     })
   });
 
-//   app.post('/api/tables', (req, res) => {
-//     let isTableReserved = false;
-//     tables
-//       .getAll()
-//       .then((tableData) => {
-//         // check if table maximum has been reached
-//         isTableReserved = tableData.length < 5;
-//         if (isTableReserved) {
-//           // add to tables and return promise
-//           return tables.push(req.body);
-//         }
-//         // add to waiting list and return promise
-//         return waitingList.push(req.body);
-//       })
-//       .then(() => {
-//         res.json(isTableReserved);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         return res.status(500).end();
-//       });
+
+//post the notes to the list
+app.post("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      let note = req.body;
+      note.id = nanoid();
+      notes.push(note);
+      fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
+        if (err) throw err;
+        res.json(notes);
+      });
+    });
+  });
+    
+    // let isTableReserved = false;
+    // tables
+    //   .getAll()
+    //   .then((tableData) => {
+    //     // check if table maximum has been reached
+    //     isTableReserved = tableData.length < 5;
+    //     if (isTableReserved) {
+    //       // add to tables and return promise
+    //       return tables.push(req.body);
+    //     }
+    //     // add to waiting list and return promise
+    //     return waitingList.push(req.body);
+    //   })
+    //   .then(() => {
+    //     res.json(isTableReserved);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     return res.status(500).end();
+    //   });
 //   });
 
   // This code isn't part of the assignment. This route was added to easily
